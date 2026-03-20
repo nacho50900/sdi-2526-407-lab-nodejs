fd5cc022fbda3d0c7b312a94ab44d49c93d5fb87
Ignacio <ignaciohoyosdiego@gmail.com>
2026-03-20 15:51:22 +0100

    SDI–2526-407- sesión8-ejercicio-complementario1

---
+++ musicstoreapp/routes/authors.js
@@ -0,0 +1,42 @@
+module.exports = function(app) {
+
+    app.get("/authors", function(req, res) {
+        let authors = [{
+            "name": "Fito",
+            "group": "Fito y los Fitipaldis",
+            "rol": "cantante"
+        },{
+            "name": "Fito",
+            "group": "Fito y los Fitipaldis",
+            "rol": "cantante"
+        }, {
+            "name": "Fito",
+            "group": "Fito y los Fitipaldis",
+            "rol": "cantante"
+        }];
+
+        let response = {
+            seller: 'Tienda de canciones',
+            authors: authors
+        };
+        res.render("authors/authors.twig", response);
+    });
+
+    app.get('/authors/add', function (req, res) {
+        res.render("authors/add.twig");
+    });
+
+
+    app.post('/authors/add', function(req, res) {
+        let response= "Autor agregado: " + req.body.name + "<br>"
+            + " grupo: " + req.body.group + "<br>"
+            + " rol: " + req.body.rol
+
+        res.send(response);
+    })
+
+    //Importante que esté al final
+    app.get('/authors/*', function(req, res) {
+        res.redirect('/authors/');
+    });
+}
