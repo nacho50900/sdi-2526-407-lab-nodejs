ddb20bf17a1cf633982f182ead2bbcf702c48d25
Ignacio <ignaciohoyosdiego@gmail.com>
2026-03-20 15:59:47 +0100

    SDI–2526-407- sesión8-ejercicio-complementario2

--- musicstoreapp/routes/authors.js
+++ musicstoreapp/routes/authors.js
@@ -23,7 +23,8 @@ module.exports = function(app) {
     });
 
     app.get('/authors/add', function (req, res) {
-        res.render("authors/add.twig");
+        let roles = ['cantante', 'trompetista', 'violinista', 'saxofonista', 'pianista'];
+        res.render("authors/add.twig", { roles: roles });
     });
 
 
