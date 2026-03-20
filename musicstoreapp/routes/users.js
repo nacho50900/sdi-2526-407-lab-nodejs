547abc3ef18b15d7f83ed253585fb879544305bc
Ignacio <ignaciohoyosdiego@gmail.com>
2026-03-20 15:24:30 +0100

    SDI-2526-407-8.5-Vistas y motores de plantillas.

---
+++ musicstoreapp/routes/users.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('listado de usuarios');
+});
+
+module.exports = router;
