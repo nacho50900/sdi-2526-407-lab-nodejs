9cf1d894014b6e6be652b287d4bfc5b8a292776f
Ignacio <ignaciohoyosdiego@gmail.com>
2026-03-20 16:17:11 +0100

    SDI–2526-407- sesión8-ejercicio-complementario3

--- musicstoreapp/routes/authors.js
+++ musicstoreapp/routes/authors.js
@@ -1,19 +1,13 @@
 module.exports = function(app) {
 
     app.get("/authors", function(req, res) {
-        let authors = [{
-            "name": "Fito",
-            "group": "Fito y los Fitipaldis",
-            "rol": "cantante"
-        },{
-            "name": "Fito",
-            "group": "Fito y los Fitipaldis",
-            "rol": "cantante"
-        }, {
-            "name": "Fito",
-            "group": "Fito y los Fitipaldis",
-            "rol": "cantante"
-        }];
+        let authors = [
+            { name: "Fito", group: "Fito y los Fitipaldis", rol: "cantante" },
+            { name: "Juan", group: "Los Panchos", rol: "trompetista" },
+            { name: "Maria", group: "Solo", rol: "violinista" },
+            { name: "Carlos", group: "Jazz Band", rol: "saxofonista" },
+            { name: "Pedro", group: "Piano Bar", rol: "pianista" }
+        ];
 
         let response = {
             seller: 'Tienda de canciones',
@@ -27,7 +21,6 @@ module.exports = function(app) {
         res.render("authors/add.twig", { roles: roles });
     });
 
-
     app.post('/authors/add', function(req, res) {
         let response= "Autor agregado: " + req.body.name + "<br>"
             + " grupo: " + req.body.group + "<br>"
@@ -35,6 +28,23 @@ module.exports = function(app) {
 
         res.send(response);
     })
+    app.get('/authors/filter/:rol', function(req, res) {
+        let authors = [
+            { name: "Fito", group: "Fito y los Fitipaldis", rol: "cantante" },
+            { name: "Juan", group: "Los Panchos", rol: "trompetista" },
+            { name: "Maria", group: "Solo", rol: "violinista" },
+            { name: "Carlos", group: "Jazz Band", rol: "saxofonista" },
+            { name: "Pedro", group: "Piano Bar", rol: "pianista" }
+        ];
+        let filtered = [];
+        for (let i = 0; i < authors.length; i++) {
+            if (authors[i].rol === req.params.rol) {
+                filtered.push(authors[i]); //QUE SEA ROL DADO
+            }
+        }
+
+        res.render("authors/authors.twig", { authors: filtered });
+    });
 
     //Importante que esté al final
     app.get('/authors/*', function(req, res) {
