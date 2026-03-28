735885649e4181f1157b9dfcc7c4db97fe710393
Ignacio <ignaciohoyosdiego@gmail.com>
2026-03-28 11:03:06 +0100

    SDI-2526-407-10.3-Mensajes y alertas.

--- musicstoreapp/routes/users.js
+++ musicstoreapp/routes/users.js
@@ -17,9 +17,14 @@ module.exports = function (app, usersRepository) {
     }
     usersRepository.insertUser(user).then(userId => {
       //res.send('Usuario registrado ' + userId);
-      res.redirect('/users/login');
+      res.redirect("/users/login" +
+          "?message=Nuevo usuario registrado"+
+          "&messageType=alert-info");
     }).catch(error => {
-      res.send("Error al insertar el usuario");
+      //res.send("Error al insertar el usuario");
+      res.redirect("/users/signup" +
+          "?message=Se ha producido un error al registrar al usuario"+
+          "&messageType=alert-danger");
     });
   });
 
@@ -38,7 +43,10 @@ module.exports = function (app, usersRepository) {
     usersRepository.findUser(filter, options).then(user => {
       if (user == null ) {
         req.session.user = null;
-        res.send("Usuario no identificado");
+        //res.send("Usuario no identificado");
+        res.redirect("/users/login" +
+            "?message=Email o password incorrecto"+
+            "&messageType=alert-danger ");
       } else {
         req.session.user = user.email;
         //res.send("Usuario identificado correctamente: " + user.email);
@@ -46,7 +54,10 @@ module.exports = function (app, usersRepository) {
       }
     }).catch(error => {
       req.session.user = null;
-      res.send("Se ha producido un error al buscar el usuario: " + error)
+      //res.send("Se ha producido un error al buscar el usuario: " + error)
+      res.redirect("/users/login" +
+      "?message=Se ha producido un error al buscar el usuario"+
+      "&messageType=alert-danger ");
     })
   })
 
